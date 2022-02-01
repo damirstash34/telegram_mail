2ae7d717e2a894535b563ceb9f440dbae5f11a86
damirstash34 <62292195+damirstash34@users.noreply.github.com>
2022-02-01 21:03:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #Bot settings
-TOKEN = "5188353793:AAFETk41gZnnBNHH2CPIzKX2dR4wj7nK4-U"
-YOUTOKEN = "381764678:TEST:33035"
-SBERTOKEN = "401643678:TEST:cf058708-b3f1-4132-beff-e79e4384331f"
+TOKEN = ""
+YOUTOKEN = ""
+SBERTOKEN = ""
 reciption_mail = ""
 reciption_id = 0000000000
 m_user_mail = ""
